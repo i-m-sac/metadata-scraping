fd339fe593f382f0a25a71bd00dfe97189f0aa6f
Unknown <sachinsreek@gmail.com>
2019-06-19 02:05:33 +0530

    Modularizing parsing

--- services/metadataService.js
+++ services/metadataService.js
@@ -4,14 +4,35 @@ let path = require('path'),
 
 
 class MetadataService {
+  /**
+   * 
+   * @param {String} url 
+   */
   async getMetadata(url) {
     try {
       let html = await apiService.get(url);
-      return html;
+      let parsedTags = this.parseMetatags(html);
+      console.log(parsedTags);
+      return parsedTags;
     } catch (err) {
       console.log(err);
       throw err;
     }
+  }
+
+  parseMetatags(html) {
+    try {
+      let head = this.getHTMLHead(html);
+      return head;
+    } catch (err) {
+      console.log('Error in parsing', err);
+    }
+  }
+
+  getHTMLHead(html) {
+    let $ = cheerio.load(html);
+    let response = $('meta[property="og:image"]').html();
+    return response;
   }
 
 }
