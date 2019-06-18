b285b2fc689f5f49c3842cf9d8e85d035898d534
Unknown <sachinsreek@gmail.com>
2019-06-19 03:09:25 +0530

    Added type and url to the response

--- services/cheerioService.js
+++ services/cheerioService.js
@@ -6,18 +6,66 @@ class CheerioService {
   }
 
   getMetadataTitle() {
-    let result = this.$("meta[property='og:title']").attr("content");
-    console.log('Title', result);
-    return result;
+    let title = this.getOGTitle();
+    if (!title) {
+      title = this.getTitle();
+    }
+    return title;
+  }
+
+  getTitle() {
+    return this.$("title").text()
+  }
+
+  getOGTitle() {
+    return this.$("meta[property='og:title']").attr("content");
   }
 
   getMetadataDescription() {
+    let description = this.getOGDescription();
+    if (!description) {
+      description = this.getDescription();
+    }
+    return description;
+  }
+
+  getDescription() {
+    return this.$("[name='description']").attr("content");
+  }
+
+  getOGDescription() {
     return this.$("meta[property='og:description']").attr("content");
   }
 
   getMetadataImage() {
     return this.$("meta[property='og:image']").attr("content");
   }
+
+  getMetadataURL() {
+    let url = this.getOGURL();
+    if (!url) {
+      url = this.getURL();
+    }
+    return url;
+  }
+
+  getOGURL() {
+    return this.$("meta[property='og:url']").attr("content");
+  }
+
+  getURL() {
+    return this.$("[name='url']").attr("content");
+  }
+
+  getMetadataType() {
+    let type = this.getOGType();
+    return type;
+  }
+
+  getOGType() {
+    return this.$("meta[property='og:type']").attr("content");
+  }
+
 }
 
 module.exports = CheerioService;
--- services/metadataService.js
+++ services/metadataService.js
@@ -23,23 +23,17 @@ class MetadataService {
   parseMetatags(html) {
     try {
       let cheerioService = new CheerioService(html);
-      let title = cheerioService.getMetadataTitle();
       let ogTags = {
         title: cheerioService.getMetadataTitle(),
         description: cheerioService.getMetadataDescription(),
+        url: cheerioService.getMetadataURL(),
+        type: cheerioService.getMetadataType(),
         images: cheerioService.getMetadataImage()
       }
       return ogTags;
-      return head;
     } catch (err) {
       console.log('Error in parsing', err);
     }
-  }
-
-  getHTMLHead(html) {
-    let $ = cheerio.load(html);
-    let response = $("meta[property='og:title']").attr("content");
-    return response;
   }
 
 }
